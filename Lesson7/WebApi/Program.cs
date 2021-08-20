eec35fb77e378c91ebceff86729752eb84def969
karl-neu <karlneumann699@gmail.com>
2021-08-20 16:41:18 +0300

    Program CreateHostBuilder remove unnecessary code

--- Lesson7/WebApi/Program.cs
+++ Lesson7/WebApi/Program.cs
@@ -1,7 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using WebApi.Filters;
 
 namespace WebApi
 {
@@ -14,21 +12,6 @@ namespace WebApi
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-            .ConfigureAppConfiguration((hostingContext, configuration) =>
-            {
-                configuration.Sources.Clear();
-
-                IHostEnvironment env = hostingContext.HostingEnvironment;
-
-                configuration
-                    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true, true);
-
-                IConfigurationRoot configurationRoot = configuration.Build();
-
-                CustomValidateOptions options = new();
-                configurationRoot.GetSection(nameof(CustomValidateOptions)).Bind(options);
-            })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
